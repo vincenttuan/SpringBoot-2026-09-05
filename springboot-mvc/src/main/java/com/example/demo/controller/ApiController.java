f77d02a2b44459a063684abb2adc02297fc26ef6
FP <FP@FP>
2026-09-05 14:53:55 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 宣告 ApiController 是一個 Controller, Rest 是表示支援的路徑風格
@@ -23,6 +24,23 @@ public class ApiController {
 		return "Welcome 歡迎光臨 !" + new Date();
 	}
 	
+	/**
+	 * 2. QueryString (? 網址帶參數)
+	 * 路徑: /hello?name=John
+	 * 路徑: /hello?name=John&age=20
+	 * 路徑: /hello?name=Mary
+	 * 
+	 * 限制: name 為必要參數, age 為可選參數(初始值=18)
+	 * */
+	//@GetMapping(value = {"/hello"})
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", required = true) String username,
+						@RequestParam(value = "age", required = false, defaultValue = "18") Integer userage) {
+		
+		String result = "Hello %s 年齡: %d".formatted(username, userage);
+		
+		return result;
+	}
 	
 	
 }
