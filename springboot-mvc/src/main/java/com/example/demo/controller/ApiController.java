f862a4320b18c7203d3745a62a37b52e5215a274
FP <FP@FP>
2026-09-05 14:35:13 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -1,10 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 宣告 ApiController 是一個 Controller, Rest 是表示支援的路徑風格
 @RequestMapping("/api") // 資源分組, 相當於 @WebServlet("/api")
 public class ApiController {
+	
+	/**
+	 * 1. Welcome
+	 * 路徑: /welcome, /home
+	 * 
+	 * 網址: http://localhost:8080/api/welcome
+	 * 網址: http://localhost:8080/api/home
+	 * */
+	
+	@GetMapping(value = {"/welcome", "/home"})
+	public String welcome() {
+		return "Welcome !";
+	}
 	
 }
