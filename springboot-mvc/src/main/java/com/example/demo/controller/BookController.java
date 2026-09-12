9b8c1dc22ccd036c7f5936de842cc3a58f1c7732
FP <FP@FP>
2026-09-12 14:39:40 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/controller/BookController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/BookController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * BookController
  * ============================================
@@ -74,6 +78,10 @@ package com.example.demo.controller;
  * ============================================
  */
 
+@CrossOrigin(origins = {"http://localhost:5173"})
+@RestController
+@RequestMapping("/book")
 public class BookController {
-
+	
+	
 }
