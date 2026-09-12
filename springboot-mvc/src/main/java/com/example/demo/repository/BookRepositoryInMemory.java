5c8c069a5d79ea28a9a0bb693ac94ddd9d0e2bc5
FP <FP@FP>
2026-09-12 14:55:51 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/repository/BookRepositoryInMemory.java
+++ springboot-mvc/src/main/java/com/example/demo/repository/BookRepositoryInMemory.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.stereotype.Repository;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.model.Book;
 
 @Repository // 專門負責 "資料存取" 的元件, Spring 會自動建立該物件並進行管理
-public class BookRepositoryInMemory {
+public class BookRepositoryInMemory implements BookRepository {
 	
 	// InMemory 版
 	private static List<Book> books = new CopyOnWriteArrayList<>();
@@ -19,6 +20,36 @@ public class BookRepositoryInMemory {
 		books.add(new Book(2, "老夫子", 10.5, 30, true));
 		books.add(new Book(3, "好小子", 13.5, 40, true));
 		books.add(new Book(4, "小甜甜", 14.5, 10, false));
+	}
+
+	@Override
+	public List<Book> findAllBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Book> getBookById(Integer id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public Boolean addBook(Book book) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean updateBook(Integer id, Book book) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean deleteBookById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
